14c2aabf05a372e73fe4b3323f1e69fb78663113
sbslc2000 <sbslc2000@gmail.com>
2023-03-11 21:05:02 +0900

    edit profile

--- src/main/java/org/cau/hellspringboot/test/HelloController.java
+++ src/main/java/org/cau/hellspringboot/test/HelloController.java
@@ -1,16 +1,26 @@
 package org.cau.hellspringboot.test;
 
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
+@ToString
+@Setter
+class HelloDto {
+
+    private String name;
+    private String hello;
+}
 @Controller
 public class HelloController {
 
     @ResponseBody
-    @GetMapping("/hello")
-    public String hello() {
-
-        return "Welcome to Hell SpringBoot";
+    @PostMapping("/hello")
+    public String hello(@RequestBody HelloDto helloDto) {
+        System.out.println(helloDto);
+        return helloDto.toString();
     }
 }
